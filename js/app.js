5f4e91d77c9de4450af11e92c92a841fe43f5680
Emerson Walsh <emersonwalsh@Emersons-MacBook-Pro.local>
2020-12-01 23:44:49 -0500

    added AOS library

--- js/app.js
+++ js/app.js
@@ -1,80 +1,74 @@
-// window.onscroll = function() {
-//     makeLogoSticky();
-// };
+AOS.init();
 
-// var header = document.getElementById("logo");
-// var sticky = header.offsetTop;
+document.addEventListener('aos:in', ({ detail }) => {
+	console.log('animated in', detail);
+});
+  
+document.addEventListener('aos:out', ({ detail }) => {
+	console.log('animated out', detail);
+})
 
-// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function makeLogoSticky() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky-logo");
-//   } else {
-//     header.classList.remove("sticky-logo");
-//   }
-// }
-
-particlesJS('particles',  
-  {
-    "particles": {
-      "number": {
-        "value": 65,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 1,
-        "random": false,
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 3,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "push": {
-          "particles_nb": 4
-        }
-      }
-    },
-    "retina_detect": true,
-  }
+particlesJS('particles',
+	{
+		"particles": {
+			"number": {
+				"value": 65,
+				"density": {
+					"enable": true,
+					"value_area": 800
+				}
+			},
+			"color": {
+				"value": "#ffffff"
+			},
+			"shape": {
+				"type": "circle",
+			},
+			"opacity": {
+				"value": 0.4,
+				"random": false,
+				// "anim": {
+				// 	"enable": false,
+				// 	"speed": 1,
+				// 	"opacity_min": 0.1,
+				// 	"sync": false
+				// }
+			},
+			"size": {
+				"value": 1,
+				"random": false,
+			},
+			"line_linked": {
+				"enable": true,
+				"distance": 150,
+				"color": "#ffffff",
+				"opacity": 0.3,
+				"width": 1
+			},
+			"move": {
+				"enable": true,
+				"speed": 2,
+				"direction": "none",
+				"random": false,
+				"straight": false,
+				"out_mode": "out",
+			}
+		},
+		"interactivity": {
+			"detect_on": "canvas",
+			"events": {
+				"onclick": {
+					"enable": true,
+					"mode": "push"
+				},
+				"resize": true
+			},
+			"modes": {
+				"push": {
+					"particles_nb": 4
+				}
+			}
+		},
+		"retina_detect": true,
+	}
 );
